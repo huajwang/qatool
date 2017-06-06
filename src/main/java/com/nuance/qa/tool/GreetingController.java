aefdf1438bb9ec7fbec5b34a3a253d2082766a9a
Huajian Wang <huajian.wang@nuance.com>
2017-06-06 15:38:15 -0400

    add payment business logic;

--- src/main/java/com/nuance/qa/tool/GreetingController.java
+++ src/main/java/com/nuance/qa/tool/GreetingController.java
@@ -5,13 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
 	
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("item", new Item());
 		return "index";
+	}
+	
+	@PostMapping("/purchase")
+	@ResponseBody
+	public String purchase(@ModelAttribute Item item) {
+		System.out.println("kkadaffffffffffffffffffffffffffffff - " + item.getId() + ", " + item.getContent());
+		return "result";
 	}
 
     @GetMapping("/greeting")
---
+++ src/main/java/com/nuance/qa/tool/Item.java
@@ -0,0 +1,22 @@
+package com.nuance.qa.tool;
+
+public class Item {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
