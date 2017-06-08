8dab19778afb5644b098116fd2b9e51be588dc93
Huajian Wang <huajian.wang@nuance.com>
2017-06-08 10:55:47 -0400

    RedirectAttributes add path parameter, query parameter and flash attribute before redirect; redirect request handler can retrieve path parameter, query parameter; the containing flash attribe can then be forwarded to view for rendering;

--- src/main/java/com/nuance/qa/tool/GreetingController.java
+++ src/main/java/com/nuance/qa/tool/GreetingController.java
@@ -1,13 +1,17 @@
 package com.nuance.qa.tool;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class GreetingController extends BaseController {
@@ -22,18 +26,36 @@ public class GreetingController extends BaseController {
 	}
 	
 	@PostMapping("/purchase")
-	@ResponseBody
-	public String purchase(@ModelAttribute Item item) {
+	public String purchase(@ModelAttribute Item item, RedirectAttributes model) {
 		logger.info(item.getId() + ", " + item.getContent());
 		Order order = new Order();
 		order.setAmount(55);
 		messaging.convertAndSend("/topic/customerservice", order);
-		return "result";
+		model.addAttribute("amount", 10);
+		model.addAttribute("username", "huajian");
+		model.addFlashAttribute("order", order);
+		return "redirect:/confirm/{username}";
 	}
 	
 	@GetMapping("/order")
 	public String orders() {
 		return "order";
+	}
+	
+	@GetMapping("/confirm/{username}")
+	public String confirmOrder(HttpServletRequest request, Model model, @PathVariable String username) {
+		logger.info("the path parameter = " + username);
+		logger.info("the query parameter = " + request.getParameter("amount"));
+		if (model.containsAttribute("order")) {
+			System.out.println("contains order: ");
+		} else {
+			System.out.println("not containing order");
+		}
+		Greeting greeting = new Greeting();
+		greeting.setId(5);
+		greeting.setContent("this is from tester.");
+		model.addAttribute("greeting", greeting);
+		return "result";
 	}
 
     @GetMapping("/greeting")
